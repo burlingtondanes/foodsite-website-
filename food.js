7993ea15151864a3dbb407a443f849e42af3dfe3
TheRealNinjauser <brandon.cameron.lee@gmail.com>
2016-10-04 09:23:47 +0100

    Part 3 + JS

---
+++ food.js
@@ -0,0 +1,13 @@
+<button onclick="myFunction()">Order</button>
+
+<p id="submit"><p>
+
+<script>
+var Orders =[<prompt>,<prompt>,<prompt>,<prompt>];
+document.getElementById("submit").innerHTML = Orders;
+
+function myFunction(){
+	products.sort();
+	document.getElementById("submit").innerHTML = Orders;
+<script>	
+}
